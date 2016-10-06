d1357f70e25e428422d4b58e26690d496bf5069a
JrodManU <jrodmanu@yahoo.com>
2016-10-06 10:06:53 -0400

    added 6 started 7

---
+++ SumSquareDifference/src/com/jrodmanu/sumsquaredifference/SumSquareDifference.java
@@ -0,0 +1,19 @@
+package com.jrodmanu.sumsquaredifference;
+
+public class SumSquareDifference {
+
+    public static void main(String[] args) {
+
+        long sumSquares = 0;
+        long sum = 0;
+
+        for(int i = 1; i <= 100; i++) {
+            sumSquares += Math.pow(i, 2);
+            sum += i;
+        }
+
+        System.out.println(Math.pow(sum, 2) - sumSquares);
+
+    }
+
+}
---
+++ _10001stPrime/src/com/jrodmanu/_10001stprime/_10001stPrime.java
@@ -0,0 +1,7 @@
+package com.jrodmanu._10001stprime;
+
+public class _10001stPrime {
+
+
+
+}
