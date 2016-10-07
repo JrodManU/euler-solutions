8adc4194557bdb5c9d5f80c49dd63be0bbda55ba
JrodManU <jrodmanu@yahoo.com>
2016-10-07 09:02:01 -0400

    finished 7

--- _10001stPrime/src/com/jrodmanu/_10001stprime/_10001stPrime.java
+++ _10001stPrime/src/com/jrodmanu/_10001stprime/_10001stPrime.java
@@ -2,6 +2,30 @@ package com.jrodmanu._10001stprime;
 
 public class _10001stPrime {
 
+    public static void main(String[] args) {
 
+        int whichPrime = 10001;
+        int currentPrime = 0;
+
+        long currentNumber = 1;
+        long sqrtNumber;
+
+        boolean isPrime;
+
+        while(currentPrime < whichPrime) {
+            currentNumber++;
+            isPrime = true;
+            sqrtNumber = (long)Math.floor(Math.sqrt(currentNumber));
+            for(int i = 2; i <= sqrtNumber; i++) {
+                if(currentNumber % i == 0) {
+                    isPrime = false;
+                }
+            }
+            if(isPrime) {
+                currentPrime++;
+            }
+        }
+
+    }
 
 }
